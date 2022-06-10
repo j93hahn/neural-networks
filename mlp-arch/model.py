a0121981e704e6ca0096eeb3e3dd716abda3b7de
Joshua Ahn <joshuajahn2001@gmail.com>
2022-06-09 20:42:59 -0400

    edits to MLP model

--- mlp-arch/model.py
+++ mlp-arch/model.py
@@ -1,6 +1,10 @@
 import numpy as np
 
-class MLP():
+class Layer(): # the base code for a single layer or an MLP
     def __init__(self) -> None:
         pass
 
+
+class Sequential(Layer):
+    def __init__(self) -> None:
+        pass

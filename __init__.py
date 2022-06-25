9b716f4620f70aedf630f25b32dd9a89cee0a47c
Joshua Ahn <joshuajahn2001@gmail.com>
2022-06-25 14:26:06 -0500

    More changes to data loading:)

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .data_loaders import MNISTDataLoader, CifarDataLoader
-
-__all__ = [
-    'MNISTDataLoader', 'CifarDataLoader'
-]
--- data_loaders/loader.py
+++ data_loaders/loader.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class MNISTDataLoader():
     def mnist(self): # MNIST dataset
-        with open("mnist.pkl",'rb') as f:
+        with open("data_loaders/mnist.pkl",'rb') as f:
             mnist = pickle.load(f)
         self.train_images = mnist["training_images"]
         self.train_labels = mnist["training_labels"]
@@ -15,7 +15,7 @@ class MNISTDataLoader():
         return self
 
     def fashion_mnist(self): # fashion MNIST dataset
-        with open("fashion_mnist.pkl",'rb') as f:
+        with open("data_loaders/fashion_mnist.pkl",'rb') as f:
             mnist = pickle.load(f)
         self.train_images = mnist["training_images"]
         self.train_labels = mnist["training_labels"]
@@ -27,7 +27,7 @@ class MNISTDataLoader():
 
 class CifarDataLoader():
     def __init__(self, file) -> None:
-        self.batch = pickle.load(open(file, 'rb'), encoding='bytes')
+        self.batch = pickle.load(open("data_loaders/" + file, 'rb'), encoding='bytes')
         self.name = self.batch[b'batch_label']
         self.labels = np.array(self.batch[b'labels'])
         self.data = self.batch[b'data']

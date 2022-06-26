13f499ba4477a1046d819342c832218a847a36a2
Joshua Ahn <joshuajahn2001@gmail.com>
2022-06-26 16:23:59 -0500

    Training/testing of feed forward fixed

--- mlp-trainer.py
+++ mlp-trainer.py
@@ -13,28 +13,29 @@ model = m.Sequential(
     m.SoftMax()
 )
 
+loss = m.CrossEntropyLoss()
 
-def trainer(model):
+
+def trainer(model, loss):
     #set_trace()
     model.train
     batch_size = 1 # SGD
     epochs = 1
     iterations = int(mnist.train_images.shape[0] / batch_size)
-    loss = m.CrossEntropyLoss()
     # actual = np.zeros((10, 1))
 
     for e in range(epochs):
         for i in range(iterations):
-            prediction, denom_sum = model.forward(mnist.train_images[i][:, np.newaxis])
+            prediction = model.forward(mnist.train_images[i][:, np.newaxis])
             actual = np.zeros((10, 1)) # produce one-hot encoding
             actual[mnist.train_labels[i]] = 1
 
             error = loss.loss(prediction, actual)
-            if i <= 50: # and i <= 150:
+            if i % 1000 == 0:
                 print("iteration " + str(i) + " --------")
-                print(prediction)
-                print(denom_sum)
-                print(actual)
+                #print(prediction)
+                #print(denom_sum)
+                #print(actual)
                 print(error)
 
             model.backward(loss.backward(prediction, actual))
@@ -42,7 +43,8 @@ def trainer(model):
 
 
     #set_trace()
-    print(model.layers)
+    #print(model.layers)
+
 
 
 def tester(model):
@@ -52,14 +54,20 @@ def tester(model):
     count = 0
     for i in range(iterations):
         prediction = model.forward(mnist.test_images[i][:, np.newaxis])
+
         if np.argmax(prediction) == mnist.test_labels[i]:
             count += 1
+
+        if i % 1000 == 0:
+            print("iteration " + str(i) + " --------")
     print(count/iterations)
 
 
 def main():
-    trainer(model)
-    #tester(model)
+    #trainer(model, loss)
+    print("Starting testing now")
+    set_trace()
+    tester(model)
 
 
 if __name__ == '__main__':

ee704c4f225f6cc85391072d99671b97f25b9315
Joshua Ahn <joshuajahn2001@gmail.com>
2022-07-01 14:31:13 -0500

    SoftMaxLoss implemented

--- modules/loss.py
+++ modules/loss.py
@@ -35,3 +35,40 @@ class MSELoss(object):
 
     def type(self):
         return "Mean Squared Error Loss"
+
+
+class SoftMaxLoss(object):
+    def __init__(self) -> None:
+        """
+        Combines softmax activation with cross entropy loss (negative log likelihood
+        loss) in order to provide ease with gradient descent
+
+        Input vector size: N x C
+        Labels size: N x C, one-hot encoded
+        N = number of samples, C = number of classes (image classification task)
+
+        Forward Steps:
+        1. Compute softmax of input vector along axis = 1
+        2. Calculate log likelihood of input vector
+        3. Generate sums for each sample and return averaged log loss
+
+        Backward Steps:
+        1. The derivative is simply -1 * (_labels - exp(self._loglikelihood))
+        2. The math works out very nicely, it's a very simple derivation
+
+        Short Note: We need the [:, np.newaxis] to account for the dimensionality
+        of the input space
+        """
+        pass
+
+    def forward(self, _input, _labels):
+        _softmax = _input - _input.max(axis=1)[:, np.newaxis]
+        self._loglikelihood = self._input - np.log(np.sum(np.exp(_softmax)), axis=1)[:, np.newaxis]
+        _sums = np.sum(-self._loglikelihood * _labels, axis=1) # generate a N x 1 vector
+        return np.mean(_sums) # generate averaged log loss for all N samples
+
+    def backward(self, _input, _labels):
+        return np.exp(self._loglikelihood - _labels)
+
+    def type(self):
+        return "Softmax Log Loss"

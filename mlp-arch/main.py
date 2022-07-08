a55213bc280737697aae57b026ba29b706af2441
Joshua Ahn <joshuajahn2001@gmail.com>
2022-07-08 17:06:35 -0500

    Updates

--- mlp-arch/main.py
+++ mlp-arch/main.py
@@ -2,22 +2,14 @@ import matplotlib.pyplot as plt
 import torch
 
 
-
-
-
-def load_models():
-    m1 = torch.load('model-5.pt') # normalized input MNIST data
-    m2 = torch.load('model-6.pt') # non-normalized input MNIST data
-
-    m1p = m1.params()
-    m2p = m2.params()
-
-    return m1, m2, m1p, m2p
-
-
 def viz():
     ...
 
 
 def main():
-    m1, m2, m1p, m2p = load_models()
+    model = torch.load('model-A.pt') # load model first
+    breakpoint()
+    print(model.components())
+
+if __name__ == '__main__':
+    main()

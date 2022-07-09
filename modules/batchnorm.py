d429454cf1a8a368659cbb842d99dae52d8c7cf2
Joshua Ahn <joshuajahn2001@gmail.com>
2022-07-08 21:39:47 -0500

    forward pass implemented correctly

--- modules/batchnorm.py
+++ modules/batchnorm.py
@@ -1,56 +1,82 @@
-from .module import Module
+from module import Module
 import numpy as np
 
 
 # requires mini-batch gradient descent
-class BatchNorm(Module):
-    def __init__(self, batchsize, input_dim) -> None:
+class BatchNorm1d(Module):
+    def __init__(self, input_dim, eps=1e-5, momentum=0.1) -> None:
         super().__init__()
-        self._batchsize = batchsize # K-value
-        self._norm = (1 / self._batchsize)
-        self._n = input_dim # same size as output_dim
-        self._G = np.random.uniform(0, 1, size=(self._n, 1))
-        self._B = np.zeros_like(self._G)
-        self._eps = 1e-5
+        """
+        Input has dimension m by C, where m is the batchsize
+
+        To calculate running statistics, use the following formulas:
+        E[x] <-- np.mean(mean_beta)
+        Var[x] <-- m/(m-1)*np.mean(var_beta)
+        """
+        self.train_first = True
+        self.inf_first = True
+        self.eps = eps
+        self.momentum = momentum
+        self.count = 0
+        self.gamma = np.ones(input_dim)
+        self.beta = np.zeros(input_dim)
+        self.gradGamma = np.zeros_like(self.gamma)
+        self.gradBeta = np.zeros_like(self.beta)
 
     def forward(self, _input):
-        self._input = _input # n x K
-        self._mean = self._norm * np.sum(self._input, axis=-1)[:, np.newaxis] # n x 1
-        self._std = np.sqrt(self._norm * np.sum(np.square(self._input - self._mean), axis=-1))[:, np.newaxis] # n x 1
-        self._Zbar = (self._input - self._mean) / (self._std + self._eps)
-        self._Zhat = (self._G * self._Zbar) + self._B # output value
+        if self.train: # training batch-normalized network
+            if self.train_first: # retrieve shape of input from the first mini-batch
+                self.m = _input.shape[0] # batch-size
+                self.running_mean = np.zeros((self.m, 1))
+                self.running_var = np.zeros((self.m, 1))
+                self.train_first = False
+
+            # calculate mini-batch statistics here
+            mean = np.mean(_input, axis=1)[:, np.newaxis] # m by 1
+            var = np.mean(np.square(_input - mean), axis=1)[:, np.newaxis] # m by 1
+
+            # normalize data, then scale and shift via affine parameters
+            x_hat = (_input - mean) / np.sqrt(var + self.eps) # m by C, broadcasted
+            y = self.gamma * x_hat + self.beta # m by C, broadcasted
+
+            # update moving statistics
+            self.running_mean += mean
+            self.running_var += var
+
+            # return output values
+            self.count += 1
+            return y
+
+        else: # inference stage
+            if self.inf_first:
+                self.running_mean /= self.count
+                self.running_var /= self.count
+                self.running_var *= (self.m / (self.m - 1))
+                self.count = 0 # training is over
+                self.inf_first = False
+
+            y = self.gamma * _input/(np.sqrt(self.running_var + self.eps))
+            y += self.beta - ((self.gamma * self.running_mean) / (np.sqrt(self.running_var + self.eps)))
+            return y
 
     def backward(self, _gradPrev):
-        # schematically, dL/dB = dL/dZ_hat * dZ_hat/dB
-        # but, dZhat/dB = 1, so it's multiplied out and we get dL/dZ_hat (_gradPrev)
-        self.l_dB = _gradPrev # not sure about this calc
-        self.l_dG = np.dot(_gradPrev, self._Zbar) # not sure about this calc
+        """
 
         """
-        dL/dZ_ij = sum (from k = 1 to K) of dL/dZhat_ik * G_i/(K*self._std_i)
-        * ()
-
-        very complicated formulation
-        """
-        self._gradCurr = ...
-        return self._gradCurr
+        ...
 
     def params(self):
-        return [], []
-
-    def update_params(self, time):
-        alpha = 0.01
-        self._B -= alpha * self.l_dB
-        self._G -= alpha * self.l_dG
+        return [self.gamma, self.beta], [self.gradGamma, self.gradBeta]
 
     def type(self):
         return "Batch Normalization Layer"
 
 
-# set_trace()
-test = BatchNorm(50, 150)
 
+test = BatchNorm1d()
+breakpoint()
+test.forward(np.random.randn(40, 100))
 
-x = np.ones((150, 50))
-#set_trace()
-test.forward(x)
+test.eval()
+breakpoint()
+test.forward(np.random.randn(40, 100))

844fd4faf720a0bc28d871ba105acc15b9ba4517
Joshua Ahn <jjahn@archimedes.ttic.edu>
2022-07-12 15:59:28 -0500

    Visualize losses on test1

--- mlp-viz.py
+++ mlp-viz.py
@@ -2,13 +2,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+# access individual arrays like A['arr_0']
 test = "1"
-experiment = "A"
-save_img = "mlp/data/test" + test + "/experiment-" + experiment + ".png"
+A = np.load("mlp/data/test" + test + "/experiment-A.npz")
+B = np.load("mlp/data/test" + test + "/experiment-B.npz")
+C = np.load("mlp/data/test" + test + "/experiment-C.npz")
+D = np.load("mlp/data/test" + test + "/experiment-D.npz")
+E = np.load("mlp/data/test" + test + "/experiment-E.npz")
+F = np.load("mlp/data/test" + test + "/experiment-F.npz")
+
+base_save_img = "mlp/plots/test" + test + "/"
 
 
-def viz():
-    ...
+def viz_losses(A, B, C, D, E, F):
+    fig, ax = plt.subplots()
+    for experiment in [A, B, C, D, E, F]:
+        ax.plot(experiment['arr_5'])
+    plt.title("Comparing Loss Rates of Six Weight Init Methods on MNIST Test Set")
+    plt.xlabel("Sample number in MNIST Test Set")
+    plt.ylabel("Log Likelihood Loss for each Sample")
+    plt.legend(["Zeros", "Random", "Gaussian", "He", "Xavier", "Xavier Normed"])
+    plt.savefig(base_save_img + "test_loss.png")
+    plt.show()
 
 
 def visualizer(x, y, grad=False, layer=0):
@@ -42,4 +57,4 @@ def visualizer(x, y, grad=False, layer=0):
 
 
 if __name__ == '__main__':
-    viz()
+    viz_losses(A, B, C, D, E, F)

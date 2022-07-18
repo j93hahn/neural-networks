bffb1cf09ac7d44b34d04ed414ca646f3a043389
Joshua Ahn <joshuajahn2001@gmail.com>
2022-07-18 17:05:03 -0500

    forward pass along batch-dimension completed

--- modules/conv.py
+++ modules/conv.py
@@ -1,45 +1,7 @@
 from numpy.lib.stride_tricks import sliding_window_view
 from module import Module
+from tqdm import tqdm
 import numpy as np
-
-
-def get_im2col_indices(x_shape, field_height, field_width, padding=1, stride=1):
-    # First figure out what the size of the output should be
-    N, C, H, W = x_shape
-    assert (H + 2 * padding - field_height) % stride == 0
-    assert (W + 2 * padding - field_height) % stride == 0
-    out_height = int((H + 2 * padding - field_height) / stride + 1)
-    out_width = int((W + 2 * padding - field_width) / stride + 1)
-
-    i0 = np.repeat(np.arange(field_height), field_width)
-    i0 = np.tile(i0, C)
-    i1 = stride * np.repeat(np.arange(out_height), out_width)
-    j0 = np.tile(np.arange(field_width), field_height * C)
-    j1 = stride * np.tile(np.arange(out_width), out_height)
-    i = (i0.reshape(-1, 1) + i1.reshape(1, -1)).astype(int)
-    j = (j0.reshape(-1, 1) + j1.reshape(1, -1)).astype(int)
-
-    k = np.repeat(np.arange(C), field_height * field_width).reshape(-1, 1).astype(int)
-
-    return (k, i, j)
-
-def im2col_indices(x, field_height, field_width, padding=0, stride=1):
-    """ An implementation of im2col based on some fancy indexing """
-    # Zero-pad the input
-    p = padding
-    x_padded = np.pad(x, ((0, 0), (0, 0), (p, p), (p, p)), mode='constant')
-
-    k, i, j = get_im2col_indices(x.shape, field_height, field_width, padding,
-                                 stride)
-
-    cols = x_padded[:, k, i, j]
-    C = x.shape[1]
-    cols = cols.transpose(1, 2, 0).reshape(field_height * field_width * C, -1)
-    return cols
-
-
-def im2col(_input, stride):
-    ...
 
 
 """
@@ -75,12 +37,11 @@ class Conv2d(Module):
             raise Exception("Output channels are not divisible by groups")
         self.out_channels = out_channels
         self.feature_count = int(in_channels/groups)
-        # self.out_shape = np.floor()
 
         self.kernel_size = kernel_size # integer type
         self.stride = stride # integer type
 
-        if pad_mode not in ["zeros", "reflect", "symmetric", "mean", "median"]:
+        if pad_mode not in ["zeros", "reflect", "symmetric"]:
             raise Exception("Invalid padding mode specified")
         if not isinstance(padding, int):
             raise Exception("Padding is not of an integral type")
@@ -90,24 +51,29 @@ class Conv2d(Module):
         # initialize parameters here - modeled after PyTorch
         _k = groups/(in_channels * (kernel_size ** 2))
         self.weights = np.random.uniform(-np.sqrt(_k), np.sqrt(_k), size=(out_channels, self.feature_count, kernel_size, kernel_size))
-        self.biases = np.random.uniform(-np.sqrt(_k), np.sqrt(_k), size=out_channels)
+        self.biases = np.random.uniform(-np.sqrt(_k), np.sqrt(_k), size=out_channels)[:, np.newaxis]
         self.gradWeights = np.zeros_like(self.weights)
         self.gradBiases = np.zeros_like(self.biases)
 
     def forward(self, _input):
-        self.out_spatial_dimension = np.floor((_input.shape[-1]+2*self.padding-self.kernel_size)/self.stride + 1).astype(int)
+        # determine output spatial dimension
+        self.out_dim = np.floor((_input.shape[-1]+2*self.padding-self.kernel_size)/self.stride + 1).astype(int)
+
+        # apply padding onto the input
         if self.padding > 0:
             pad_width = ((0, 0), (0, 0), (self.padding, self.padding), (self.padding, self.padding))
             _input = np.pad(_input, pad_width, mode=self.pad_mode)
 
-        _windows = sliding_window_view(_input, window_shape=(self.kernel_size, self.kernel_size), axis=(-2, -1))[:, :, ::self.stride, ::self.stride]
+        # Method 1 - process the batch, one image at a time
+        _output = []
+        for i in range(_input.shape[0]): # process each image individually
+            _windows = sliding_window_view(_input[i], window_shape=(self.kernel_size, self.kernel_size), axis=(-2, -1))[:, ::self.stride, ::self.stride]
+            _windows = _windows.reshape(self.feature_count*(self.kernel_size**2), self.out_dim**2)
+            _curr = np.dot(self.weights.reshape(self.out_channels, self.feature_count*(self.kernel_size**2)), _windows) + self.biases
+            _output.append(_curr.reshape(self.out_channels, self.out_dim, self.out_dim))
+        _output = np.stack(_output, axis=0) # stack images along batch dimension
 
-        X_col = im2col_indices(_input, self.kernel_size, self.kernel_size, padding=self.padding, stride=self.stride)
-        W_rows = self.weights.reshape(self.feature_count, -1)
-        _output = np.dot(W_rows, X_col) + self.biases[:, np.newaxis]
-        _output = _output.reshape(self.weights.shape[0], self.out_spatial_dimension,
-                                  self.out_spatial_dimension, _input.shape[0])
-        _output = _output.transpose(3, 0, 1, 2)
+        # Method 2 - process batch through vectorization - ??
         return _output
 
     def backward(self, _input, _gradPrev):
@@ -151,19 +117,39 @@ class Flatten2d(Module):
         return "Flatten2d Layer"
 
 
-def test_conv2d():
+def test_forward_conv2d():
     import torch.nn as nn
     import torch
-    standard = nn.Conv2d(1, 6, 2, padding=1, stride=1)
-    #import torch
-    test = Conv2d(1, 6, 2, padding=1, stride=1)
-    _input = np.random.randn(1, 1, 5, 5)
-    torch_input = torch.randn(1, 1, 5, 5)
-    breakpoint()
-    s_o = standard.forward(torch_input)
-    t_o = test.forward(_input)
-    breakpoint()
-    test.backward(_input, 0)
+    for i in tqdm(range(100)):
+        # generate 100 random sample architectures to test forward pass
+        batch_size = np.random.randint(1, 100)
+        in_channels = np.random.randint(1, 20) * 24
+        out_channels = np.random.randint(10, 23) * 24
+        groups = 1
+        #groups = np.random.choice((1, 2, 3, 4, 6, 8, 12, 24))
+        padding = np.random.randint(0, 5)
+        stride = np.random.randint(1, 10)
+        kernel_size = np.random.randint(15, 22)
+
+        correct = nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                            kernel_size=kernel_size,
+                            groups=groups, padding=padding, stride=stride)
+        testing = Conv2d(in_channels=in_channels, out_channels=out_channels,
+                            kernel_size=kernel_size,
+                            groups=groups, padding=padding, stride=stride)
+        x = np.arange(batch_size*in_channels*kernel_size*kernel_size).reshape(
+                      batch_size, in_channels, kernel_size, kernel_size)
+
+        correct_output = correct(torch.tensor(x, dtype=torch.float)).detach().numpy()
+        testing_output = testing.forward(x)
+        assert correct_output.shape == testing_output.shape
+
+    print("Done testing forward pass :)")
+
+
+def test_backward_conv2d():
+    import torch.nn as nn
+    import torch
 
 
 def test_flatten2d():
@@ -177,9 +163,6 @@ def test_flatten2d():
 
 
 if __name__ == '__main__':
-    breakpoint()
-    x = np.random.randn(5, 1, 10, 10)
-    y = im2col_indices(x, 3, 3, 1, 1)
-    breakpoint()
-    test_conv2d()
+    test_forward_conv2d()
+    #test_backward_conv2d()
     #test_flatten2d()
